c752542abec7efb532a135b0706d5efc50e70c8d
hsnyderiii <hbsnyderiii@alaska.edu>
2024-09-18 11:48:27 -0800

    Add GetNodes
    Adds the function GetNodes which reads each node into an array and then appends said array into a main array called node_arr.

--- convert.py
+++ convert.py
@@ -36,7 +36,38 @@ def GetUnits(lines):
     
     return units_text
 
+def GetNodes(lines):
+    node_arr = []
+    node_flag = False
+    for line in lines:
+        if '[NODES]' in line:
+            node_flag = True
+        elif '[END_NODES]' in line:
+            node_flag = False
+        elif node_flag == True:
+            curr_node = line.split('   ')
+            curr_len = len(curr_node)
+            flag_num = 0
+            while (flag_num < curr_len):    
+                if curr_node[flag_num] == '' or curr_node[flag_num] == '  ':
+                    curr_node.remove(curr_node[flag_num])
+                    curr_len -= 1
+                elif '"' in curr_node[flag_num]:
+                    curr_node[flag_num] = curr_node[flag_num].replace('"', '')
+                else:
+                    flag_num += 1
+            end_of_curr_node = curr_node[flag_num - 1].split(' ')
+            curr_node.remove(curr_node[flag_num - 1])
+            for flag in end_of_curr_node:
+                if ';' in flag:
+                    flag = flag.split(';')[0]
+                curr_node.append(flag)
+            node_arr.append(curr_node)
+    return node_arr
+
 with open("2024 SB V4.5.r3d") as file:
     data=file.read().split('\n')
     print(GetUnits(data))
+    print(GetNodes(data))
+
     #print(data)
