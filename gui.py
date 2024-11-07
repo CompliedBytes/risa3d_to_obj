2e6fdfbdc4fe73d90b9acff1c4d2150e79c1ab54
CompiledBytes <jamiebum12@live.com>
2024-11-07 10:59:01 -0900

    made on_select and convert take in the file as an argument and fixed the code in the appropriate places.
    changed bg_color to all caps to show that it is a constant value

--- gui.py
+++ gui.py
@@ -3,25 +3,25 @@ from tkinter import *
 from tkinter import filedialog
 
 
-def on_select():
+def on_select(file):
     selected_file = filedialog.askopenfilename()
     if selected_file:
         file.set(selected_file)
         file_label.config(text=selected_file)
 
-def convert():
+def convert(file):
     print(file.get())
 
-bg_color = 'lightblue'
+BACKGROUND_COLOR = 'lightblue'
 
 
 root = Tk()
 root.title("RISA-3D to OBJ Converter")
 style = ttk.Style(root)
-style.configure('UFrame', background=bg_color, foreground='black')
-style.configure('TFrame', background=bg_color, foreground='black')
-style.configure('TLabel', background=bg_color, foreground='black')
-style.configure('TCheckbutton', background=bg_color)
+style.configure('UFrame', background=BACKGROUND_COLOR, foreground='black')
+style.configure('TFrame', background=BACKGROUND_COLOR, foreground='black')
+style.configure('TLabel', background=BACKGROUND_COLOR, foreground='black')
+style.configure('TCheckbutton', background=BACKGROUND_COLOR)
 
 mainframe = ttk.Frame(root, padding="3 3 12 12")
 mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
@@ -32,7 +32,7 @@ ttk.Label(mainframe, text="UAA 3D File Conversion Tool", font=("Arial", 15)).gri
 
 file = StringVar()
 file_frame = ttk.Frame(mainframe)
-file_button = ttk.Button(file_frame, text="Select File", command=on_select)
+file_button = ttk.Button(file_frame, text="Select File", command=lambda: on_select(file))
 file_label = ttk.Label(file_frame, text="                                                         ", background="white", relief='sunken')
 file_button.grid(column=0, row=0, sticky = 'W')# side='left')
 file_label.grid(column=1, row=0, padx=(5,0), pady=(0,0))
@@ -65,7 +65,7 @@ bottom_button = ttk.Checkbutton(options_frame, text="Bottom", variable=side, onv
 options_frame.grid(column=0, row=4, padx=(0,0), pady=(10,10))
 
 bottom_frame = ttk.Frame(mainframe)
-convert_button = ttk.Button(bottom_frame, text="Convert", command=convert).grid(column=0, row=0, padx=(0,25), pady=(0,0))
+convert_button = ttk.Button(bottom_frame, text="Convert", command =lambda: convert(file)).grid(column=0, row=0, padx=(0,25), pady=(0,0))
 exit_button = ttk.Button(bottom_frame, text="Exit", command=root.destroy).grid(column=1, row=0, padx=(25,0), pady=(0,0))
 bottom_frame.grid(column=0, row=5, padx=(0,0), pady=(10,5))
 
