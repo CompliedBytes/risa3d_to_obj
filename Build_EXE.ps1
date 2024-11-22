1fcc6d7475ea5f19beebf9c1ee87afa2bc9a3521
hsnyderiii <hbsnyderiii@alaska.edu>
2024-11-22 12:54:15 -0900

    Added an icon to the exe

--- Build_EXE.ps1
+++ Build_EXE.ps1
@@ -1,3 +1,3 @@
-pyinstaller --onefile -w  'convert.py'
+pyinstaller --add-data "Icon.ico;." --icon=Icon.ico --onefile -w  'convert.py'
 
-pyinstaller -w  'convert.py'
+pyinstaller --add-data "Icon.ico;." --icon=Icon.ico -w  'convert.py'
